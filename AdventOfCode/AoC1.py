63052826e9f952d306618751dc53c9f6e1865e0b
GowthamSagar310 <gowthamsagartummeda@gmail.com>
2023-12-01 18:39:22 +0530

    added AdventOfCode AoC1

---
+++ AdventOfCode/AoC1.py
@@ -0,0 +1,33 @@
+# first part
+f = open("../../input.txt" ,'r')
+lines = [line.strip() for line in f.readlines()]
+digits = ["".join([l for l in line if l.isdigit()]) for line in lines]
+print(sum([int(d[0] + d[-1]) for d in digits]))
+
+# second part
+keys = [
+    ("zero", "0"), 
+    ("one", "1"), 
+    ("two", "2"), 
+    ("three", "3"), 
+    ("four", "4"), 
+    ("five", "5"), 
+    ("six", "6"), 
+    ("seven", "7"), 
+    ("eight", "8"), 
+    ("nine", "9")
+]
+s = 0
+for line in lines:
+    current_index = 0
+    while current_index < len(line):
+        temp = line[current_index:]
+        for i in range(10):
+            if len(temp) >= len(keys[i][0]):
+                if temp.startswith(keys[i][0]):
+                    line = line.replace(keys[i][0], keys[i][1] + keys[i][0][-1])
+        current_index += 1
+    digits = "".join([l for l in line if l.isdigit()])
+    digits = digits[0] + digits[-1]
+    s += int(digits)
+print(s)
